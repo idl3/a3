1130e094882e49f801c4e68e23cc6b17c7449501
Ernest Sim <ernest.sim@actatlys.com>
2012-05-04 22:09:09 +0800

    Fixed styling for businesses & admin businesses, users and adding banned functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter do |c|
+    c.send(:banned?, current_user)
+  end
+
+  def banned?(current_user)
+    if current_user.try(:banned) and request.path != "/"
+      flash[:alert] = "You are banned"
+      sign_out(current_user)
+      redirect_to root_path
+    end
+  end
 end
